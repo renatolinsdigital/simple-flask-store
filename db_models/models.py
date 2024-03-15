628fa7e9c44d0a11f31f90e87bf7462cb43a3a0b
Renato Lins <renato.lins.digital@outlook.com>
2024-03-15 19:10:36 -0300

    first commit

---
+++ db_models/models.py
@@ -0,0 +1,17 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    product_id = db.Column(db.Integer, nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    total_price = db.Column(db.Float, nullable=False)
+    user_email = db.Column(db.String(100), nullable=False)
