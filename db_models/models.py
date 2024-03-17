1e65453acddb49a5df48c5fcb10eb7570c836f73
Renato Lins <renato.lins.digital@outlook.com>
2024-03-17 18:44:33 -0300

    updates the data model and UI

--- db_models/models.py
+++ db_models/models.py
@@ -4,14 +4,67 @@ db = SQLAlchemy()
 
 
 class Product(db.Model):
+    # Product model representing a product in the store
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
     price = db.Column(db.Float, nullable=False)
+    code = db.Column(db.String(50), unique=True, nullable=False)
+
+    # Define a one-to-many relationship with CartItem model
+    cart_items = db.relationship('CartItem', backref='product', lazy=True)
+
+    def __repr__(self):
+        return f'<Product {self.name}>'
 
 
-class Order(db.Model):
+class CartItem(db.Model):
+    # CartItem model representing an item in a shopping cart
     id = db.Column(db.Integer, primary_key=True)
-    product_id = db.Column(db.Integer, nullable=False)
     quantity = db.Column(db.Integer, nullable=False)
     total_price = db.Column(db.Float, nullable=False)
-    user_email = db.Column(db.String(100), nullable=False)
+
+    # Define foreign key relationships with Product and Cart models
+    product_id = db.Column(db.Integer, db.ForeignKey(
+        'product.id'), nullable=False)
+    cart_id = db.Column(db.Integer, db.ForeignKey('cart.id'), nullable=False)
+
+    # # Define many-to-one relationships with Product and Cart models
+    # product = db.relationship(
+    #     'Product', backref=db.backref('cart_items', lazy=True))
+    # cart = db.relationship('Cart', backref=db.backref('items', lazy=True))
+
+    def __repr__(self):
+        return f'<CartItem {self.quantity} of {self.product.name}>'
+
+
+class Cart(db.Model):
+    # Cart model representing a user's shopping cart
+    id = db.Column(db.Integer, primary_key=True)
+    total_price = db.Column(db.Float, nullable=False)
+
+    # Define a foreign key relationship to the User model
+    user_id = db.Column(db.Integer, db.ForeignKey(
+        'user.id'), unique=True, nullable=False)
+
+    # # Define a one-to-one relationship with the User model
+    # user = db.relationship('User', backref='cart', uselist=False, lazy=True)
+
+    # Define a one-to-many relationship with CartItem model
+    items = db.relationship('CartItem', backref='cart', lazy=True)
+
+    def __repr__(self):
+        return f'<Cart {self.id}>'
+
+
+class User(db.Model):
+    # User model representing a registered user
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(50), unique=True, nullable=False)
+    email = db.Column(db.String(255), unique=True, nullable=False)
+    name = db.Column(db.String(255), nullable=False)
+
+    # Define a one-to-one relationship with the Cart model
+    cart = db.relationship('Cart', backref='user', uselist=False, lazy=True)
+
+    def __repr__(self):
+        return f'<User {self.id}>'
--- routes/routes.py
+++ routes/routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
-from db_models.models import db, Product, Order
+from sqlalchemy.exc import IntegrityError  # Import IntegrityError
+from db_models.models import db, Product, CartItem, Cart, User
 
 # Create a Blueprint instance for routes
 bp = Blueprint('routes', __name__)
@@ -9,10 +10,24 @@ bp = Blueprint('routes', __name__)
 
 @bp.route('/')
 def index():
+    # Check if a user exists in the database
+    user = User.query.first()
+    if not user:
+        # If no user is found, create a new user
+        new_user = User(
+            user_name='admin',
+            email='user@admin.com',
+            name='John Doe The Admin'
+        )
+        db.session.add(new_user)
+        db.session.commit()
+        # Reload the user object to ensure it's properly loaded
+        user = User.query.first()
+
     # Retrieve all products from the database
     products = Product.query.all()
     # Render the index.html template with the products data
-    return render_template('index.html', products=products)
+    return render_template('index.html', products=products, user=user)
 
 # Route to add a product to the cart
 
@@ -24,19 +39,29 @@ def add_to_cart(product_id):
     # Retrieve the quantity of the product from the form data
     quantity = int(request.form['quantity'])
 
+    # Get the current user
+    user = User.query.first()  # TODO: Must retrieve the logged-in user
+
+    # Check if the user has a cart, create one if not
+    if not user.cart:
+        cart = Cart(total_price=0.0, user=user)
+        db.session.add(cart)
+        db.session.commit()
+
     # Check if the product is already in the cart
-    existing_order = Order.query.filter_by(product_id=product_id).first()
-    if existing_order:
-        # Update the quantity and total price of the existing order
-        existing_order.quantity += quantity
-        existing_order.total_price = existing_order.quantity * product.price
+    cart_item = CartItem.query.filter_by(
+        product_id=product_id, cart_id=user.cart.id).first()
+    if cart_item:
+        # Update the quantity and total price of the existing cart item
+        cart_item.quantity += quantity
+        cart_item.total_price = cart_item.quantity * product.price
     else:
-        # Calculate the total price for the new order
+        # Calculate the total price for the new cart item
         total_price = product.price * quantity
-        # Create a new order in the database
-        order = Order(product_id=product_id, quantity=quantity,
-                      total_price=total_price, user_email='example@email.com')
-        db.session.add(order)
+        # Create a new cart item in the database
+        cart_item = CartItem(product_id=product_id, quantity=quantity,
+                             total_price=total_price, cart_id=user.cart.id)
+        db.session.add(cart_item)
 
     # Commit the changes to the database
     db.session.commit()
@@ -50,17 +75,30 @@ def add_to_cart(product_id):
 
 @bp.route('/create_product', methods=['GET', 'POST'])
 def create_product():
-    if request.method == 'POST':
-        # Retrieve product name and price from the form data
+    # Check if the request method is POST and the user is 'admin'
+    if request.method == 'POST' and User.query.filter_by(user_name='admin').first():
+        # Retrieve product code, name, and price from the form data
+        code = request.form['code']
         name = request.form['name']
         price = float(request.form['price'])
+
         # Create a new product in the database
-        product = Product(name=name, price=price)
+        product = Product(name=name, price=price, code=code)
         db.session.add(product)
-        db.session.commit()
-        # Redirect to the home page
-        return redirect(url_for('routes.index'))
-    # Render the create_product.html template for GET requests
+
+        try:
+            db.session.commit()  # Try committing the changes
+            # Flash a success message
+            flash('Product created successfully!', 'success')
+        except IntegrityError:
+            db.session.rollback()  # Rollback the transaction
+            # Flash a message indicating the code already exists
+            flash('Product code already exists. Please choose a different code.', 'error')
+
+        # Reloads as the proper way is having the function returning something
+        return render_template('create_product.html')
+
+    # Render the create_product.html template for GET requests or unauthorized POST requests
     return render_template('create_product.html')
 
 # Route for the shopping cart
@@ -68,20 +106,27 @@ def create_product():
 
 @bp.route('/cart')
 def cart():
-    # Retrieve all items in the cart from the database
-    cart_items = Order.query.all()
-    # Calculate the total cart price
-    total_cart_price = sum(item.total_price for item in cart_items)
+    # TODO: Retrieve the logged-in user's cart
+    user_cart = User.query.first().cart
 
-    # Ensure each Order object has a reference to its corresponding Product
-    for item in cart_items:
-        item.product = Product.query.get(item.product_id)
+    if user_cart:
+        # Retrieve all cart items associated with the user's cart
+        cart_items = CartItem.query.filter_by(cart_id=user_cart.id).all()
+        # Calculate the total cart price
+        total_cart_price = sum(item.total_price for item in cart_items)
 
-    # Render the cart.html template with the cart items and total cart price
-    return render_template('cart.html', cart_items=cart_items, total_cart_price=total_cart_price)
+        # Ensure each CartItem object has a reference to its corresponding Product
+        for item in cart_items:
+            item.product = Product.query.get(item.product_id)
+
+        # Render the cart.html template with the cart items and total cart price
+        return render_template('cart.html', cart_items=cart_items, total_cart_price=total_cart_price)
+    else:
+        # Render the cart.html template with an empty cart message
+        return render_template('cart.html', cart_items=[], total_cart_price=0)
+
 
 # Return the Blueprint object for registration in app.py
-
 
 def create_routes_blueprint():
     return bp
